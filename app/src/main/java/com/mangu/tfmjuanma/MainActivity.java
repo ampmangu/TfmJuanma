e69bb6fbbd23b1afbece4220f34dd14d45d91286
Adrian Marin <adrianmarinportillo@gmail.com>
2024-02-04 19:45:27 +0100

    wip(): Added basic RecapActivity, and navigation from MainActivity

--- app/src/main/java/com/mangu/tfmjuanma/MainActivity.java
+++ app/src/main/java/com/mangu/tfmjuanma/MainActivity.java
@@ -1,18 +1,35 @@
 package com.mangu.tfmjuanma;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mangu.tfmjuanma.databinding.ActivityMainBinding;
 
 import dagger.hilt.android.AndroidEntryPoint;
-import dagger.hilt.android.HiltAndroidApp;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        initializeView();
+    }
+
+    private void initializeView() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        initializeButtons();
+    }
+
+    private void initializeButtons() {
+        binding.btnRecap.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, RecapActivity.class)));
     }
 }
